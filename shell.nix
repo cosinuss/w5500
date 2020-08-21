5a1ebc08bb6a6975aa029403861f77b71a93da10
Sam Lakerveld <dark@dark.red>
2020-08-21 16:39:08 +0200

    shell.nixx: update rust

--- shell.nix
+++ shell.nix
@@ -1,19 +1,13 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  nixpkgs = import <nixpkgs> {
-    overlays = [ moz_overlay ];
-  };
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+
+  rust-channel = (nixpkgs.rustChannelOf { date = "2020-07-27"; channel = "nightly"; });
 in
   with nixpkgs;
   stdenv.mkDerivation {
-    name = "hub";
+    name = "w5500";
     buildInputs = [
-      ((rustChannelOf { date = "2020-03-28"; channel = "nightly"; }).rust.override {
-        targets = [
-          "x86_64-unknown-linux-gnu"
-        ];
-        extensions = [ "rust-src" "rust-std" ];
-      })
+      rust-channel.rust
     ];
   }
-
